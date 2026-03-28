40e1fee589ea788e7165d8d5613b9b8d27f91eb3
Magico <keming_wang@outlook.com>
2026-03-28 17:26:46 -0400

    add env local

--- src/app/api/experiences/route.js
+++ src/app/api/experiences/route.js
@@ -1,20 +1,23 @@
-import { NextResponse } from 'next/server';
-import { MongoClient } from 'mongodb';
+import { NextResponse } from "next/server";
+import { MongoClient } from "mongodb";
 
-const uri = "mongodb+srv://techico:<Wkm20150117>@cluster0.zubauvj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = process.env.MONGODB_URI; // Ensure this is set in your environment variables
 const client = new MongoClient(uri);
 
 export async function GET() {
   try {
     await client.connect();
-    const db = client.db('Magico-Portfolio'); // change this
-    const collection = db.collection('coco-experience');
+    const db = client.db("Magico-Portfolio"); // change this
+    const collection = db.collection("coco-experience");
     const data = await collection.find({}).toArray();
 
     return NextResponse.json(data);
   } catch (error) {
     console.error(error);
-    return NextResponse.json({ error: 'Failed to fetch experiences' }, { status: 500 });
+    return NextResponse.json(
+      { error: "Failed to fetch experiences" },
+      { status: 500 },
+    );
   } finally {
     await client.close();
   }
--- src/db.js
+++ src/db.js
@@ -1,10 +1,10 @@
 // lib/db.js
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const MONGODB_URI = "mongodb+srv://techico:Wkm20150117@cluster0.zubauvj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
-  throw new Error('Please define MONGODB_URI in .env.local');
+  throw new Error("Please define MONGODB_URI in .env.local");
 }
 
 let cached = global.mongoose || { conn: null, promise: null };
@@ -13,14 +13,16 @@ export async function connectToDatabase() {
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, {
-      bufferCommands: false,
-    }).then((mongoose) => {
-      return mongoose;
-    });
+    cached.promise = mongoose
+      .connect(MONGODB_URI, {
+        bufferCommands: false,
+      })
+      .then((mongoose) => {
+        return mongoose;
+      });
   }
 
   cached.conn = await cached.promise;
   return cached.conn;
 }
-if (process.env.NODE_ENV !== 'production') global.mongoose = cached;
+if (process.env.NODE_ENV !== "production") global.mongoose = cached;
